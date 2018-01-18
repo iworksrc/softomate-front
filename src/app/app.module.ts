70d66af98c8a1117b715c32f2e0772365be4b0f5
Роман Киселёв <imail.for.iwork@gmail.com>
2018-01-18 13:59:32 +0700

    Covalent and AngularMaterial libs  and his themes installed and configured as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,32 @@ import { CoreComponent } from './modules/core/components/core/core.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './routes';
 
+import {
+  CovalentLayoutModule,
+  CovalentStepsModule
+} from '@covalent/core';
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 @NgModule({
   declarations: [
   ],
   imports: [
     BrowserModule,
     CoreModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [CoreComponent]
